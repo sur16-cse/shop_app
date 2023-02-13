63fecc9b6983299dd5a7f8dd4fe291041eb581bb
Surbhi <surbhikumaridav@gmail.com>
2023-02-13 21:42:32 +0530

    futuure builder

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -15,41 +15,51 @@ class OrdersScreen extends StatefulWidget {
 }
 
 class _OrdersScreenState extends State<OrdersScreen> {
-  var _isLoading = false;
+  late Future _ordersFuture;
+
+  Future _obtainOrdersFuture() {
+    return Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
+  }
 
   @override
   void initState() {
-    Future.delayed(Duration.zero).then((_) async {
-      setState(() {
-        _isLoading = true;
-      });
-      await Provider.of<Orders>(context, listen: false).fetchAndSetOrders();
-      setState(() {
-        _isLoading = false;
-      });
-    });
+    _ordersFuture = _obtainOrdersFuture();
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
-    final orderData = Provider.of<Orders>(context);
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Orders'),
       ),
       drawer: const AppDrawer(),
-      body: _isLoading
-          ? const Center(
+      body: FutureBuilder(
+        builder: (ctx, dataSnapshot) {
+          if (dataSnapshot.connectionState == ConnectionState.waiting) {
+            return const Center(
               child: SpinKitPouringHourGlassRefined(
                 color: Colors.purple,
                 size: 50.0,
               ),
-            )
-          : ListView.builder(
-              itemBuilder: (ctx, i) => OrderItemWidget(orderData.orders[i]),
-              itemCount: orderData.orders.length,
-            ),
+            );
+          } else {
+            if (dataSnapshot.error != null) {
+              return const Center(
+                child: Text('An error occured.'),
+              );
+            } else {
+              return Consumer<Orders>(
+                builder: (ctx, orderData, child) => ListView.builder(
+                  itemBuilder: (ctx, i) => OrderItemWidget(orderData.orders[i]),
+                  itemCount: orderData.orders.length,
+                ),
+              );
+            }
+          }
+        },
+        future: _ordersFuture,
+      ),
     );
   }
 }
