d0ea893e1060f3e024e405ec779605b07fc60605
Surbhi <surbhikumaridav@gmail.com>
2023-02-16 08:20:09 +0530

    auto logout when token expires

--- lib/providers/auth.dart
+++ lib/providers/auth.dart
@@ -1,13 +1,15 @@
 import 'dart:convert';
+import 'dart:async';
 
 import 'package:flutter/widgets.dart';
 import 'package:http/http.dart' as http;
 import 'package:shop_app/models/http_exception.dart';
 
 class Auth with ChangeNotifier {
-   String? _token;
-   DateTime? _expiryDate;
-   String? _userId;
+  String? _token;
+  DateTime? _expiryDate;
+  String? _userId;
+  Timer? _authTimer;
 
   bool get isAuth {
     return token != null;
@@ -22,7 +24,7 @@ class Auth with ChangeNotifier {
     return null;
   }
 
-  String? get userId{
+  String? get userId {
     return _userId;
   }
 
@@ -57,6 +59,7 @@ class Auth with ChangeNotifier {
           seconds: int.parse(responseData["expiresIn"]),
         ),
       );
+      _autoLogout();
       notifyListeners();
     } catch (error) {
       // TODO
@@ -72,13 +75,22 @@ class Auth with ChangeNotifier {
     return _authenticate(email, password, "signInWithPassword");
   }
 
-  void logout(){
-    _token=null;
-    _userId=null;
-    _expiryDate=null;
+  void logout() {
+    _token = null;
+    _userId = null;
+    _expiryDate = null;
+    if(_authTimer!=null){
+      _authTimer!.cancel();
+      _authTimer=null;
+    }
     notifyListeners();
-}
+  }
 
-
-
+  void _autoLogout() {
+    if(_authTimer!=null){
+      _authTimer!.cancel();
+    }
+    final timeToExpiry = _expiryDate?.difference(DateTime.now()).inSeconds;
+    _authTimer = Timer(Duration(seconds: timeToExpiry!), logout);
+  }
 }
